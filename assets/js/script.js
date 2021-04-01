ac11868d7567b03a62ca92e2fcde97238fd3f72c
General Kenobi <78252449+mmgleave@users.noreply.github.com>
2021-04-01 15:25:35 -0600

    fix validation of length prompt

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,74 +1,11 @@
-// Assignment code here 
-
-// confirm number of characters user needs
-// function getCharacterLength() {
-//   var userLength = prompt("Please enter the number of characters needed (8-128):");
-//   userLength = parseInt(userLength);
-//   console.log(typeof(userLength));
-//   if(userLength < 8 || userLength > 128) {
-//     alert("Please enter a valid input.")
-//     getCharacterLength();
-//   } else{
-//     console.log("User defined length is: " + userLength);
-//   };
-// }
-
-// // confirm if user needs LOWERCASE characters
-// function getLowercaseConfirm() {
-//   var userLowercase = prompt("Are lowercase characters needed? (Enter 'Y' or 'N')");
-//   userLowercase = userLowercase.toLowerCase();
-//   if(userLowercase === "y" || userLowercase === "n"){
-//     console.log("User defined lowercase: " + userLowercase);
-//   } else{
-//     alert("Please enter a valid input.");
-//     getLowercaseConfirm();
-//   };
-// };
-
-// // confirm if user needs UPPERCASE characters
-// function getUppercaseConfirm() {
-//   var userUppercase = prompt("Are uppercase characters needed? (Enter 'Y' or 'N')");
-//   userUppercase = userUppercase.toLowerCase();
-//   if(userUppercase === "y" || userUppercase === "n"){
-//     console.log("User defined uppercase: " + userUppercase);
-//   } else{
-//     alert("Please enter a valid input.");
-//     getUppercaseConfirm();
-//   };
-// };
-
-// // confirm if user needs NUMERIC characters
-// function getNumericConfirm() {
-//   var userNumeric = prompt("Are numeric characters needed? (Enter 'Y' or 'N')");
-//   userNumeric = userNumeric.toLowerCase();
-//   if(userNumeric === "y" || userNumeric === "n"){
-//     console.log("User defined uppercase: " + userNumeric);
-//   } else{
-//     alert("Please enter a valid input.");
-//     getNumericConfirm();
-//   };
-// };
-
-// // confirm if user needs NUMERIC characters
-// function getSpecialConfirm() {
-//   var userSpecial = prompt("Are special characters needed? (Enter 'Y' or 'N')");
-//   userSpecial = userSpecial.toLowerCase();
-//   if(userSpecial === "y" || userSpecial === "n"){
-//     console.log("User defined uppercase: " + userSpecial);
-//   } else{
-//     alert("Please enter a valid input.");
-//     getSpecialConfirm();
-//   };
-// };
-
-
 function generatePassword() {
   // user length prompt
   var userLength = prompt("Please enter the number of characters needed (8-128):");
-  if(userLength < 8 || userLength > 128 ) {
-    alert("Please enter a number between 8 and 128.")
+  if(userLength > 7 && userLength < 129 ) {
+    console.log(userLength);
+  } else {
+    alert("Please enter a valid number between 8 and 128.");
     generatePassword();
-  } else {console.log(userLength)
   };
 
   // lower case confirm
@@ -114,10 +51,8 @@ function generatePassword() {
     newPassword += passwordString[Math.floor(Math.random() * passwordString.length)];
   }
   console.log(newPassword);
-  
+  return(newPassword);
 };
-
-generatePassword();
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
